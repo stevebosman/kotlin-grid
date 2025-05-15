cf28953ab6ed822644043e9a0d3f726d991627c6
Steve Bosman <stevebosman@gmail.com>
2025-05-15 22:45:34 +0100

    ci: add build and publish workflows
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,29 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("java-library")
+    id("maven-publish")
 }
 
 group = "uk.co.stevebosman.grid"
-version = "1.0-SNAPSHOT"
+version = "0.0.2"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/stevebosman/kotlin-grid")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
 
 repositories {
     mavenCentral()
