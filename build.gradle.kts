47fc366f50d2570ff211fcac4be83e19d2ce1df0
Steve Bosman <stevebosman@gmail.com>
2025-05-17 23:49:31 +0100

    ci: fix publish attempt - again?
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 group = "uk.co.stevebosman.grid"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.2"
 
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/stevebosman/grid")
+            url = uri("https://maven.pkg.github.com/stevebosman/kotlin-grid")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
