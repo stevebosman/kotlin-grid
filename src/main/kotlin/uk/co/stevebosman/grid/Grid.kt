45248c5de693f5b9395127a4ab8f61cacd101ba0
Steve Bosman <stevebosman@gmail.com>
2025-06-08 23:48:51 +0100

    doc: colourise examples
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

--- src/main/kotlin/uk/co/stevebosman/grid/Grid.kt
+++ src/main/kotlin/uk/co/stevebosman/grid/Grid.kt
@@ -9,10 +9,10 @@ data class Grid(
     val xRange: IntRange,
     val yRange: IntRange
 ) {
-    fun toSvg(scaling: Int = 25, border: Int = 5, showXIndex: Boolean = false, showYIndex: Boolean = false): String {
-        fun adjustedX(v: Point): Double = border + scaling * (v.x - boundingBox.lowerLeft.x)
+    fun toSvg(options: SvgOptions = SvgOptions()): String {
+        fun adjustedX(v: Point): Double = options.border + options.scaling * (v.x - boundingBox.lowerLeft.x)
 
-        fun adjustedY(v: Point): Double = border + scaling * (v.y - boundingBox.lowerLeft.y)
+        fun adjustedY(v: Point): Double = options.border + options.scaling * (v.y - boundingBox.lowerLeft.y)
 
         fun coordinates(v: Point): String =
             "${adjustedX(v)},${adjustedY(v)}"
@@ -20,25 +20,28 @@ data class Grid(
         val result = StringBuilder()
         result.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n")
         result.append("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n")
-        result.append("<svg width=\"${scaling * boundingBox.width + 2 * border}\" height=\"${scaling * boundingBox.height + 2 * border}\"")
-        result.append(" viewBox=\"0 0 ${2 * border + scaling * boundingBox.width} ${2 * border + scaling * boundingBox.height}\"")
+        result.append("<svg width=\"${options.scaling * boundingBox.width + 2 * options.border}\" height=\"${options.scaling * boundingBox.height + 2 * options.border}\"")
+        result.append(" viewBox=\"0 0 ${2 * options.border + options.scaling * boundingBox.width} ${2 * options.border + options.scaling * boundingBox.height}\"")
         result.append(" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n")
-        if (showXIndex || showYIndex) {
-            result.append("    <style>.small {font: italic ${scaling/5}px sans-serif;}</style>")
+        if (options.showXIndex || options.showYIndex) {
+            result.append("    <style>.coordinates {font: italic ${options.scaling / 5}px sans-serif;}</style>\n")
         }
 
         cells.values.forEach { c ->
             val polygon = c.getPolygon()
-            result.append("    <polyline points=\"")
-            result.append(polygon.vertices.joinToString(" ") { v -> coordinates(v) })
-            result.append(" " + coordinates(polygon.vertices.first()))
-            result.append("\" stroke=\"red\" stroke-linecap=\"round\" stroke-width=\"2\" fill=\"none\" />\n")
-            if (showXIndex || showYIndex) {
-                result.append("   <text x=\"${adjustedX(polygon.centre)-scaling/5}\" y=\"${adjustedY(polygon.centre)}\" class=\"small\">")
-                if (showXIndex) result.append("${c.gridReference.x}")
-                if (showXIndex&&showYIndex) result.append(",")
-                if (showYIndex) result.append("${c.gridReference.y}")
-                result.append("</text>")
+            result.append("    <polygon")
+            result.append(" points=\"${polygon.vertices.joinToString(" ") { v -> coordinates(v) }}\"")
+            result.append(" stroke=\"${options.lineColour}\"")
+            result.append(" stroke-linecap=\"${options.lineCap}\"")
+            result.append(" stroke-width=\"${options.lineWidth}\"")
+            result.append(" fill=\"${options.fill(c.gridReference)}\"")
+            result.append("/>\n")
+            if (options.showXIndex || options.showYIndex) {
+                result.append("   <text x=\"${adjustedX(polygon.centre) - options.scaling / 5}\" y=\"${adjustedY(polygon.centre)}\" class=\"coordinates\">")
+                if (options.showXIndex) result.append("${c.gridReference.x}")
+                if (options.showXIndex && options.showYIndex) result.append(",")
+                if (options.showYIndex) result.append("${c.gridReference.y}")
+                result.append("</text>\n")
             }
         }
         result.append("</svg>\n")
