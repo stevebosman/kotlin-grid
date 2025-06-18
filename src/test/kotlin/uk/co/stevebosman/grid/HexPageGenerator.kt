380f89a0a74af244a3ee79682b259925088050f3
Steve Bosman <stevebosman@gmail.com>
2025-06-18 22:24:03 +0100

    feat: hex page examples
    Signed-off-by: Steve Bosman <stevebosman@gmail.com>

---
+++ src/test/kotlin/uk/co/stevebosman/grid/HexPageGenerator.kt
@@ -0,0 +1,19 @@
+package uk.co.stevebosman.grid
+
+import uk.co.stevebosman.grid.impl.regular.hex.HexGridGenerator
+import uk.co.stevebosman.grid.impl.regular.hex.HexGridOption
+import java.io.File
+
+fun main() {
+    val hexagonalSvgOptions = SvgOptions(borderColour = "black")
+
+    File("examples/pages/hex_30_25.svg").writeText(
+        HexGridGenerator.generate(30, 25, HexGridOption.STANDARD_SKIP_LAST).toSvg(hexagonalSvgOptions)
+    )
+    File("examples/pages/hex_20_17.svg").writeText(
+        HexGridGenerator.generate(20, 17, HexGridOption.STANDARD_SKIP_LAST).toSvg(hexagonalSvgOptions)
+    )
+    File("examples/pages/hex_10_9.svg").writeText(
+        HexGridGenerator.generate(10, 9, HexGridOption.STANDARD_SKIP_LAST).toSvg(hexagonalSvgOptions)
+    )
+}
